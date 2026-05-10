f6a2e5cf909de61ace0d1fb624de163fe03d0a05
Kamlesh Kumar <67237252+kamleshsande85@users.noreply.github.com>
2026-05-10 11:58:22 +0530

    Serve React frontend for all unmatched paths
    Added a re_path to serve the React frontend for all other paths.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -10,3 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('scanner.urls')),
 ]
+
+# Serve React Frontend (for all other paths)
+re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
